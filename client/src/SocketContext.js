d2afe2a2a3496ef130d23800fad2ee72566b34c7
ArzuGoyushova <arzusg@code.edu.az>
2023-09-10 17:33:57 +0400

    refactor: Vall App is updated.

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -3,7 +3,7 @@ import {io} from 'socket.io-client';
 import Peer from 'simple-peer';
 
 const SocketContext = createContext();
-const socket = io('http://localhost:5000');
+const socket = io('https://vall-app.up.railway.app');
 
 const ContextProvider = ({children}) => {
     const [stream, setStream] = useState(null);
